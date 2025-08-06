be438a3ce4517f5a691589f391ca5020d14034ae
playtimegaby23 <playtimegaby23@gmail.com>
2025-08-05 21:20:22 -0300

    Eliminados secretos del repo, actualizada .gitignore y ajustes en run.py

--- run.py
+++ run.py
@@ -6,11 +6,9 @@ app = create_app()
 
 # Ejecutar solo si este archivo es el punto de entrada
 if __name__ == "__main__":
-    # Obtener el puerto desde variables de entorno o usar 5000 por defecto
     port = int(os.environ.get("PORT", 5000))
+    
+    # Railway siempre maneja DEBUG=False por defecto para producción
+    debug = os.environ.get("FLASK_DEBUG", "False").lower() == "true"
 
-    # Definir si se ejecuta en modo debug (por defecto True si no se define DEBUG)
-    debug = os.environ.get("FLASK_DEBUG", "True").lower() == "true"
-
-    # Ejecutar la app
     app.run(host="0.0.0.0", port=port, debug=debug)
